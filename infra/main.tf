c0b7ce2420894324d9e76e4faa826a2e5d1505b3
Malviya <pranjal.malviya@capgemini.com>
2025-06-09 12:49:46 +0530

    route-table-added

--- infra/main.tf
+++ infra/main.tf
@@ -4,9 +4,21 @@ provider "aws" {
  
 resource "aws_vpc" "main_vpc" {
 cidr_block = "10.0.0.0/16"
+enable_dns_hostnames = true
+enable_dns_support = true
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "main-igw"
+  }
+  
+}
+
  
-resource "aws_subnet" "main_subnet" {
+resource "aws_subnet" "main_subnet1" {
 vpc_id = aws_vpc.main_vpc.id
 cidr_block = "10.0.1.0/24"
 map_public_ip_on_launch = true
@@ -16,7 +28,7 @@ availability_zone = "us-east-1a"
     }
 }
 
-resource "aws_subnet" "main_subnet1" {
+resource "aws_subnet" "main_subnet2" {
   vpc_id = aws_vpc.main_vpc.id
   cidr_block = "10.0.2.0/24"
   map_public_ip_on_launch = true
@@ -24,6 +36,24 @@ resource "aws_subnet" "main_subnet1" {
         tags = {
             Name = "Public Subnet2"
         }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+  
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main_subnet1.id
+  route_table_id = aws_route_table.public_rt.id
+  
 }
  
 resource "aws_security_group" "sg" {
@@ -61,7 +91,8 @@ vpc_id = aws_vpc.main_vpc.id
 resource "aws_instance" "jenkins_instance" {
   ami                         = "ami-0c101f26f147fa7fd" 
   instance_type               = "t2.micro"
-subnet_id = aws_subnet.main_subnet.id
+subnet_id                     = aws_subnet.main_subnet1.id
+
 vpc_security_group_ids = [aws_security_group.sg.id]
   key_name                    = "Vilgax" 
   associate_public_ip_address = true
