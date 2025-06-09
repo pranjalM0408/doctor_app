1f348a7b4fb9f588465c2407b9fe352af9e127b1
Malviya <pranjal.malviya@capgemini.com>
2025-06-09 14:27:23 +0530

    rds-endpoint

--- node_app/db.js
+++ node_app/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host: 'terraform-20250609070906694100000002.clvc3zyttfjo.us-east-1.rds.amazonaws.com:3306',
+  host: 'terraform-20250609085312416700000004.cib03qptj6yg.us-east-1.rds.amazonaws.com:3306',
   user: 'admin',
   password: 'admin1234',
   database: 'appointments'
